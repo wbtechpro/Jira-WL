bf29af84fe7bd4d7c6a7f367e52e54ff7a1a7b2c
Kira Berengolts <berengoltskir@gmail.com>
2022-04-13 18:22:51 +0300

    Change the logic of tuesdays_date_update
    Also change "automatic_date_update" to "28th_date_update" and change the logic of the command

--- api/client/management/commands/tuesdays_date_update.py
+++ api/client/management/commands/tuesdays_date_update.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from datetime import datetime
-from dateutil.relativedelta import *
 
 from client.models import PeriodForDownloadModel
 
@@ -8,8 +7,7 @@ from client.models import PeriodForDownloadModel
 class Command(BaseCommand):
     """
     Обновляет конечную дату для промежуточного (раз в неделю ночью с понедельника на вторник) скачивания ворклогов:
-    прибавляет неделю к дате предыдущего вторника. Так, если дата предыдущего вторника - 2022-04-12,
-    то дата следующего - 2022-04-19.
+    ставит такой датой текущую, если в этот день вторник.
     Количество дней для скачивания ворклогов - 90. Его можно будет в любой момент поменять в админке
     """
 
@@ -17,10 +15,9 @@ class Command(BaseCommand):
         today = datetime.today()
         day_of_week = today.weekday()
         if day_of_week == 1 and today.day != 28:
-            next_tuesday = today + relativedelta(weeks=1)
+            model_instance = PeriodForDownloadModel.objects.get(pk=1)
+            model_instance.end_date = today
+            model_instance.days = 90
+            model_instance.save()
         else:
-            return None
-        model_instance = PeriodForDownloadModel.objects.get(pk=1)
-        model_instance.end_date = next_tuesday
-        model_instance.days = 90
-        model_instance.save()
+            return
